9cd43a2fd062d54d3f2d7bf10fed135708a38e5a
Michael Pankov <work@michaelpankov.com>
2015-11-06 18:58:18 +0300

    Accept GitLab port argument

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,10 @@ fn main() {
         .author("Mikhail Pankov <mikhail.pankov@kaspersky.com>")
         .about("A commit gatekeeper for SDK")
         .args_from_usage(
-            "-n --dry-run 'Don\'t actually do anything, just print what is to be done'")
+            "-n --dry-run 'Don\'t actually do anything, just print what is to be done'
+             -g --gitlab-port=[GITLAB_PORT] 'Port to listen for GitLab WebHooks'")
         .get_matches();
 
     println!("Dry run: {}", matches.is_present("dry-run"));
+    println!("GitLab port: {}", matches.value_of("GITLAB_PORT").unwrap_or("10042"));
 }
