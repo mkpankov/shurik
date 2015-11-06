972a760d74a314a49dc38876e3da88bf90137472
Michael Pankov <work@michaelpankov.com>
2015-11-06 20:27:08 +0300

    Implement a prototype that gets WebHook content

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,38 @@
 extern crate clap;
+extern crate iron;
+extern crate router;
+
 use clap::App;
+use iron::*;
+
+use std::io::Read;
+
+#[derive(Debug)]
+struct MyError;
+
+impl std::fmt::Display for MyError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "")
+    }
+}
+
+impl std::error::Error for MyError {
+    fn description(&self) -> &str {
+        ""
+    }
+}
+
+fn handle_mr(req: &mut Request) -> IronResult<Response> {
+    let ref mut body = req.body;
+    let mut s: String = String::new();
+    body.read_to_string(&mut s).unwrap();
+
+    println!("{}", req.url);
+    println!("{}", s);
+
+    return Ok(Response::with(status::Ok));
+    Err(iron::error::IronError::new(MyError, status::BadRequest))
+}
 
 fn main() {
     let matches =
@@ -9,9 +42,23 @@ fn main() {
         .about("A commit gatekeeper for SDK")
         .args_from_usage(
             "-n --dry-run 'Don\'t actually do anything, just print what is to be done'
-             -g --gitlab-port=[GITLAB_PORT] 'Port to listen for GitLab WebHooks'")
+             -p --gitlab-port=[GITLAB_PORT] 'Port to listen for GitLab WebHooks'
+             -a --gitlab-address=[GITLAB_ADDRESS] 'Address to listen for GitLab WebHooks'")
         .get_matches();
 
     println!("Dry run: {}", matches.is_present("dry-run"));
-    println!("GitLab port: {}", matches.value_of("GITLAB_PORT").unwrap_or("10042"));
+    let gitlab_port =
+        matches.value_of("GITLAB_PORT").unwrap_or("10042").parse().unwrap_or(10042);
+    let gitlab_address =
+        matches.value_of("GITLAB_ADDRESS").unwrap_or("localhost");
+    println!("GitLab port: {}", gitlab_port);
+    println!("GitLab address: {}", gitlab_address);
+
+    let mut router = router::Router::new();
+    router.post("/api/v1/mr",
+                move |req: &mut Request|
+                handle_mr(req));
+    Iron::new(router).http(
+        (gitlab_address, gitlab_port))
+        .expect("Couldn't start the web server");
 }
