b7050791b86a5323aed8470c53c792212e643bed
Michael Pankov <work@michaelpankov.com>
2015-11-06 18:39:55 +0300

    Add the arguments parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate clap;
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    let matches =
+        App::new("shurik")
+        .version("0.1")
+        .author("Mikhail Pankov <mikhail.pankov@kaspersky.com>")
+        .about("A commit gatekeeper for SDK")
+        .args_from_usage(
+            "-n --dry-run 'Don\'t actually do anything, just print what is to be done'")
+        .get_matches();
+
+    println!("Dry run: {}", matches.is_present("dry-run"));
 }
