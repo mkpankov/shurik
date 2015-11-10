df5afa9700b29d92772c037bcb1abcf08faa4371
Michael Pankov <work@michaelpankov.com>
2015-11-10 16:01:40 +0300

    Get path to script

--- ci/run.sh
+++ ci/run.sh
@@ -2,8 +2,14 @@
 
 set -e
 
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPT_DIR=$(dirname "$SCRIPT")
+echo "$SCRIPT_DIR"
+
 RUST_INSTALL_DIR="$WORKSPACE/install/rust"
-./rustup.sh --prefix="$RUST_INSTALL_DIR" --disable-sudo --yes
+"$SCRIPT_DIR/rustup.sh" --prefix="$RUST_INSTALL_DIR" --disable-sudo --yes
 export PATH="RUST_INSTALL_DIR/bin:$PATH"
 export LD_LIBRARY_PATH="RUST_INSTALL_DIR/lib:$LD_LIBRARY_PATH"
 
