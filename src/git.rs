fc3373d2bbdcd65b5456801f20e686d232edfca0
Michael Pankov <work@michaelpankov.com>
2015-11-13 16:43:52 +0300

    Move git module to file of its own

---
+++ src/git.rs
@@ -0,0 +1,86 @@
+use ::std::process::{Command, ExitStatus};
+
+pub fn fetch() {
+    let status = Command::new("git")
+        .arg("fetch")
+        .current_dir("workspace/shurik")
+        .status()
+        .unwrap_or_else(|e| {
+            panic!("failed to execute process: {}", e)
+        });
+    if ExitStatus::success(&status) {
+        println!("Fetched remote");
+    } else {
+        panic!("Couldn't fetch remote: {}", status)
+    }
+}
+
+pub fn checkout(branch: &str) {
+    let status = Command::new("git")
+        .arg("checkout").arg(branch)
+        .current_dir("workspace/shurik")
+        .status()
+        .unwrap_or_else(|e| {
+            panic!("failed to execute process: {}", e)
+        });
+    if ExitStatus::success(&status) {
+        println!("Checked out {}", branch);
+    } else {
+        panic!("Couldn't checkout the {} branch: {}", branch, status)
+    }
+}
+
+pub fn reset_hard(to: &str) {
+    let status = Command::new("git")
+        .arg("reset").arg("--hard").arg(to)
+        .current_dir("workspace/shurik")
+        .status()
+        .unwrap_or_else(|e| {
+            panic!("failed to execute process: {}", e)
+        });
+    if ExitStatus::success(&status) {
+        println!("Reset 'try' to {}", to);
+    } else {
+        panic!("Couldn't reset the 'try' branch: {}", status)
+    }
+}
+
+pub fn push(do_force: bool) {
+    let mut command = Command::new("git");
+    let builder = command
+        .arg("push");
+
+    if do_force {
+        builder.arg("--force-with-lease");
+    }
+
+    let builder = builder
+        .current_dir("workspace/shurik");
+
+    let status = builder
+        .status()
+        .unwrap_or_else(|e| {
+            panic!("failed to execute process: {}", e)
+        });
+    if ExitStatus::success(&status) {
+        println!("Push current branch");
+    } else {
+        panic!("Couldn't push the current branch: {}", status)
+    }
+}
+
+pub fn merge_ff(mr_human_number: u64) {
+    let status = Command::new("git")
+        .arg("merge").arg("try").arg("--ff-only")
+        .arg(&*format!("-m \"Merging MR #{}\"", mr_human_number))
+        .current_dir("workspace/shurik")
+        .status()
+        .unwrap_or_else(|e| {
+            panic!("failed to execute process: {}", e)
+        });
+    if ExitStatus::success(&status) {
+        println!("Merge master to MR {}", mr_human_number);
+    } else {
+        panic!("Couldn't merge the 'master' branch: {}", status)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,8 @@ use std::io::Read;
 use std::sync::{Arc, Condvar, Mutex};
 use std::thread;
 
+mod git;
+
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 enum SubStatusOpen {
     WaitingForReview,
@@ -325,96 +327,6 @@ fn handle_comment(req: &mut Request, queue: &(Mutex<LinkedList<MergeRequest>>, C
 
     println!("handle_comment finished      : {}", time::precise_time_ns());
     return Ok(Response::with(status::Ok));
-}
-
-
-mod git {
-    use ::std::process::{Command, ExitStatus};
-
-    pub fn fetch() {
-        let status = Command::new("git")
-            .arg("fetch")
-            .current_dir("workspace/shurik")
-            .status()
-            .unwrap_or_else(|e| {
-                panic!("failed to execute process: {}", e)
-            });
-        if ExitStatus::success(&status) {
-            println!("Fetched remote");
-        } else {
-            panic!("Couldn't fetch remote: {}", status)
-        }
-    }
-
-    pub fn checkout(branch: &str) {
-        let status = Command::new("git")
-            .arg("checkout").arg(branch)
-            .current_dir("workspace/shurik")
-            .status()
-            .unwrap_or_else(|e| {
-                panic!("failed to execute process: {}", e)
-            });
-        if ExitStatus::success(&status) {
-            println!("Checked out {}", branch);
-        } else {
-            panic!("Couldn't checkout the {} branch: {}", branch, status)
-        }
-    }
-
-    pub fn reset_hard(to: &str) {
-        let status = Command::new("git")
-            .arg("reset").arg("--hard").arg(to)
-            .current_dir("workspace/shurik")
-            .status()
-            .unwrap_or_else(|e| {
-                panic!("failed to execute process: {}", e)
-            });
-        if ExitStatus::success(&status) {
-            println!("Reset 'try' to {}", to);
-        } else {
-            panic!("Couldn't reset the 'try' branch: {}", status)
-        }
-    }
-
-    pub fn push(do_force: bool) {
-        let mut command = Command::new("git");
-        let builder = command
-            .arg("push");
-
-        if do_force {
-            builder.arg("--force-with-lease");
-        }
-
-        let builder = builder
-            .current_dir("workspace/shurik");
-
-        let status = builder
-            .status()
-            .unwrap_or_else(|e| {
-                panic!("failed to execute process: {}", e)
-            });
-        if ExitStatus::success(&status) {
-            println!("Push current branch");
-        } else {
-            panic!("Couldn't push the current branch: {}", status)
-        }
-    }
-
-    pub fn merge_ff(mr_human_number: u64) {
-        let status = Command::new("git")
-            .arg("merge").arg("try").arg("--ff-only")
-            .arg(&*format!("-m \"Merging MR #{}\"", mr_human_number))
-            .current_dir("workspace/shurik")
-            .status()
-            .unwrap_or_else(|e| {
-                panic!("failed to execute process: {}", e)
-            });
-        if ExitStatus::success(&status) {
-            println!("Merge master to MR {}", mr_human_number);
-        } else {
-            panic!("Couldn't merge the 'master' branch: {}", status)
-        }
-    }
 }
 
 mod jenkins {
