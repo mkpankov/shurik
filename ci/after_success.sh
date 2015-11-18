7d994062a786cfa0535a570658d6c8c69f313810
Michael Pankov <work@michaelpankov.com>
2015-11-18 16:41:54 +0300

    Kill the process before copying new executable

--- ci/after_success.sh
+++ ci/after_success.sh
@@ -8,8 +8,10 @@ set -x
 
 HOST='user@host'
 
+ssh "$HOST" 'pkill shurik'
+
 scp ./ci/run.sh "$HOST:"
 scp ./target/debug/shurik "$HOST:"
 ssh "$HOST" 'ssh-keyscan gitlab.host > ~/.ssh/known_hosts'
 
-ssh "$HOST" 'pkill shurik || sh -c "nohup \"./run.sh\" > shurik.out 2>&1 < /dev/null &"'
+ssh "$HOST" 'sh -c "nohup \"./run.sh\" > shurik.out 2>&1 < /dev/null &"'
