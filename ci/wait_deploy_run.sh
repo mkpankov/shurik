ae84f005f4d2187be957abaccadc80a21c5e4767
Michael Pankov <work@michaelpankov.com>
2015-11-18 18:11:19 +0300

    Debug

--- ci/wait_deploy_run.sh
+++ ci/wait_deploy_run.sh
@@ -2,15 +2,26 @@
 set -e
 set -u
 
+echo 1
 while ! lsof | egrep -q "\b$PWD/shurik_new\b"; do
+    echo 2
     sleep 1;
+    echo 3
 done;
+echo 4
 
+echo 5
 sleep 10
+echo 6
 
+echo 7
 pkill shurik
+echo 8
 mv shurik_new shurik
+echo 9
 
+echo 10
 RUST_BACKTRACE=1
 export RUST_BACKTRACE
 ./shurik
+echo 11
