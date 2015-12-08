cb838e6d75446aa36b04c294db6d3e594ce31ab6
Michael Pankov <work@michaelpankov.com>
2015-12-08 15:35:49 +0300

    Add build result debugging output

--- src/main.rs
+++ src/main.rs
@@ -820,6 +820,7 @@ fn handle_build_request(
                 },
                 ref r => panic!("Expected status to be waiting for result dispatch, but got {:?}", r),
             }
+            info!("Result: {}", result_string);
             if request.approval_status == ApprovalStatus::Approved
                 && result_string == "SUCCESS"
             {
