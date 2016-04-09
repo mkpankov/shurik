60a5f12b26ab491f5f4c7dc68aa3fa018b788e86
Michael Pankov <work@michaelpankov.com>
2016-04-09 17:15:34 +0300

    Change library paths

--- ci/after_success.sh
+++ ci/after_success.sh
@@ -13,8 +13,8 @@ if [ "$RUN_TYPE" = "deploy" ]; then
 
     scp ./ci/wait_deploy_run.sh "$HOST:"
     scp ./target/debug/shurik "$HOST:shurik_new"
-    scp /lib/libssl.so.1.0.0 "$HOST:"
-    scp /lib/libcrypto.so.1.0.0 "$HOST:"
+    scp /usr/lib/x86_64-linux-gnu/libssl.so.1.0.0 "$HOST:"
+    scp /usr/lib/x86_64-linux-gnu/libcrypto.so.1.0.0 "$HOST:"
 
     ssh "$HOST" 'sh -c "mktemp > logname"'
     ssh "$HOST" 'sh -c "nohup \"./wait_deploy_run.sh\" > $(cat logname) 2>&1 < /dev/null &"'
